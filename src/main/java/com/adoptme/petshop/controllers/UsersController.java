8315e2266c5ab795f84ebfb212422480ec4cb7c5
dcirer <domy.741@gmail.com>
2024-07-06 12:45:42 -0300

    UsersController

---
+++ src/main/java/com/adoptme/petshop/controllers/UsersController.java
@@ -0,0 +1,90 @@
+package com.adoptme.petshop.controllers;
+
+import com.adoptme.petshop.entities.User;
+import com.adoptme.petshop.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UsersController {
+    @Autowired  UsersService service;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        try {
+            User newUser = service.save(user);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> readAll(){
+        try {
+            List<User> users = service.readAll();
+            return ResponseEntity.ok(users);
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> read(@PathVariable Long id){
+        try {
+            Optional<User> user = service.readOne(id);
+            if (user.isPresent()){
+                return ResponseEntity.ok(user.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User data){
+        try {
+            Optional<User> optionalUser = service.readOne(id);
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                if (data.getName() != null) {
+                    user.setName(user.getName());
+                }
+                if (data.getEmail() != null) {
+                    user.setEmail(user.getEmail());
+                }
+                service.save(user);
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<User> destroy(@PathVariable Long id){
+        try {
+            Optional<User> user =service.destroyOne(id);
+            if (user.isPresent()){
+                return ResponseEntity.ok(user.get());
+            }else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
